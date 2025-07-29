648ebc74ce58dbdcdfbc541fcea976b8f20d209a
giri.nam <giri.nam@coinone.com>
2025-07-29 11:37:50 +0900

    change init.sql

--- mysql/mysql8/init.sql
+++ mysql/mysql8/init.sql
@@ -10,3 +10,15 @@ GRANT REPLICATION SLAVE ON *.* TO 'ndgndg91'@'%';
 FLUSH PRIVILEGES;
 
 SHOW GRANTS FOR 'ndgndg91'@'%';
+     
+-- 데이터베이스 생성 (원하는 데이터베이스 이름으로 변경하세요)
+CREATE DATABASE my_sandbox;
+
+-- 'ndgndg91' 계정에 'my_sandbox'에 대한 모든 권한 부여
+GRANT ALL PRIVILEGES ON my_sandbox.* TO 'ndgndg91'@'%';
+
+-- 변경된 권한을 즉시 적용
+FLUSH PRIVILEGES;
+
+-- 'ndgndg91' 계정의 현재 권한 확인 (선택 사항)
+SHOW GRANTS FOR 'ndgndg91'@'%';
