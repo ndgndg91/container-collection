861438bcc5377a9c837e6d8a7e57535fd38b1c40
ndgndg91 <ndgndg91@gmail.com>
2025-10-07 19:56:52 +0900

    remove es curl example

--- es/practice/delete/delete.sh
+++
@@ -1,2 +0,0 @@
-curl -i -X DELETE "https://localhost:9200/my_index/_doc/1?pretty" \
--u elastic:ndgndg91 -k
--- es/practice/read/get/get.sh
+++
@@ -1,2 +0,0 @@
-curl -i -X GET "https://localhost:9200/my_index/_doc/1" \
--u elastic:ndgndg91 -k
